230066f42feac0f82eb4c6dcd12961a9094d691c
Kyle <shaman16x@gmail.com>
2014-03-29 10:13:50 -0700

    Added most of the frame handling functions

--- IntermediateTree/frame.h
+++ IntermediateTree/frame.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "util.h"
 #include "symbol.h"
 #include "temp.h"
@@ -6,15 +7,11 @@
 
 typedef struct F_frame_ *F_frame;
 typedef struct F_access_ *F_access;
-
 typedef struct F_accessList_ *F_accessList;
 
-/*
-struct F_accessList_ {
-	F_access head;
-	F_accessList tail;
-};
-*/
+struct F_frame_ {Temp_label name; F_accessList formals;};
+struct F_accessList_ {F_access head;F_accessList tail;};
+
 F_frame F_newFrame(Temp_label name, U_boolList formals);
 Temp_label F_name(F_frame frame);
 F_accessList F_formals(F_frame frame);
