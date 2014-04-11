1c15cb515dd67d2655b889ddc93279b98eac7aa5
Shaman16x <shaman16x@gmail.com>
2014-04-10 19:17:49 -0500

    removed some comments

--- Assembly/mipscodegen.c
+++ Assembly/mipscodegen.c
@@ -61,51 +61,6 @@ static Temp_temp munchExp(T_exp e){
 					L(r, NULL), L(munchExp(e1), NULL), NULL));
 			return r;
 		}
-
-		case MEM(CONST(i)): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("LOAD 'd0 <- M[r0+" + i + "]\n",
-					L(r, NULL), NULL), NULL));
-			return r;
-		}
-
-		case MEM(e1): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("LOAD 'd0 <- M['s0+0]\n",
-					L(r, NULL), L(munchExp(e1), NULL), NULL));
-			return r;
-		}
-
-		case BINOP(PLUS, e1, CONST(i)): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("ADDI 'd0 <- 's0+" + i + "\n",
-					L(r, NULL), L(munchExp(e1), NULL), NULL));
-			return r;
-		}
-
-		case BINOP(PLUS, CONST(i), e1): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("ADDI 'd0 <- 's0+" + i + "\n",
-					L(r, NULL), L(munchExp(e1), NULL), NULL));
-			return r;
-		}
-
-		case CONST(i): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("ADDI 'd0 <- r0+" + i + "\n",
-					NULL, L(munchExp(e1), NULL), NULL));
-			return r;
-		}
-
-		case BINOP(PLUS, e1, e2): {
-			Temp_temp r = Temp_newtemp();
-			emit(AS_Oper("ADD 'd0 <- 's0+'s1\n",
-					L(r, NULL), L(munchExp(e1), L(munchExp(e2), NULL)), NULL));
-			return r;
-		}
-
-		case TEMP(t):
-			return t;
         */
         case T_MEM:{
             T_exp mem = e->u.MEM;
